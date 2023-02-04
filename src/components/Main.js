489406656c57441c98a50d120e3fcd4e5f61534f
18137036 <9wjdgh9@naver.com>
2023-02-04 19:32:31 +0900

    Feat : Main component 구현

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
+const MainWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .main-container {
+    position: relative;
+    margin-right: 67px;
+    margin-left: 67px;
+    margin-top: 65px;
+    margin-bottom: 75px;
+    box-sizing: border-box;
+  }
+`;
 function Main(props) {
-    return (
-        <div>
-          <h1>메인</h1>
-        </div> 
-    );
+  return (
+    <main>
+      <MainWrapper>
+        <div className="main-container"></div>
+      </MainWrapper>
+    </main>
+  );
 }
 
 export default Main;
