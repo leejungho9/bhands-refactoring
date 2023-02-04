82fe8f0aef0d9384e2032de73163b9f112c86cc6
18137036 <9wjdgh9@naver.com>
2023-02-04 19:07:37 +0900

    Feat : header component 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,58 @@
 import React from "react";
+import styled from "styled-components";
 
+const HeaderWrapper = styled.div`
+  .header-container {
+    background-color: red;
+    width: 100%;
+    height: 114px;
+    margin: 0 auto;
+    position: relative;
+
+    .logo{
+        width: 160px;
+        height: 25px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+        
+    }
+
+    .ham{
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+        display: flex;
+        align-items: center;
+        margin-right: 70px;
+        cursor: pointer;
+
+        img{
+            width: 30px;
+            height: 25px;
+        }
+    }
+  }
+`;
 function Header(props) {
   return (
-    <div>
-      <h1>헤더</h1>
-    </div>
+    <HeaderWrapper>
+      <header>
+        <div className="header-container">
+          <a href="/" className="logo">
+            <img src="./icons/logo.png"alt="로고" />
+          </a>
+          <div className="ham">
+            <img src="./icons/ham.png" alt="햄버거버튼"/>
+          </div>
+        </div>
+      </header>
+    </HeaderWrapper>
   );
 }
 
