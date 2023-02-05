da0c1886a0ce51789f939fb723eb648007a5bc43
18137036 <9wjdgh9@naver.com>
2023-02-05 23:18:41 +0900

    Feat : header component html, css 구현 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,55 +2,59 @@ import React from "react";
 import styled from "styled-components";
 
 const HeaderWrapper = styled.div`
-  .header-container {
-    background-color: red;
-    width: 100%;
-    height: 114px;
-    margin: 0 auto;
-    position: relative;
-
-    .logo{
-        width: 160px;
-        height: 25px;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        margin: auto;
-        
-    }
-
-    .ham{
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        margin: auto;
-        display: flex;
-        align-items: center;
-        margin-right: 70px;
-        cursor: pointer;
-
-        img{
-            width: 30px;
-            height: 25px;
-        }
-    }
-  }
+  border-bottom: 1px solid #b5b5b6;
 `;
+const TopBar = styled.div`
+  height: 10px;
+  background-color: #000;
+`;
+const  HeaderContainer = styled.div`
+  width: 100%;
+  height: 114px;
+  margin: 0 auto;
+  position: relative;
+`
+const LogoIcon = styled.a`
+  width: 160px;
+  height: 25px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+`
+const HamIcon = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  margin-right: 70px;
+  cursor: pointer;
+`
+
+const LogoImg = styled.img``
+const HamImg = styled.img`
+  width: 30px;
+  height: 25px;
+`
+
 function Header(props) {
   return (
     <HeaderWrapper>
+      <TopBar/>
       <header>
-        <div className="header-container">
-          <a href="/" className="logo">
-            <img src="./icons/logo.png"alt="로고" />
-          </a>
-          <div className="ham">
-            <img src="./icons/ham.png" alt="햄버거버튼"/>
-          </div>
-        </div>
+        <HeaderContainer>
+          <LogoIcon>
+            <LogoImg src="./icons/logo.png"alt="로고" />
+          </LogoIcon>
+          <HamIcon>
+            <HamImg src="./icons/ham.png" alt="햄버거버튼"/>
+          </HamIcon>
+        </HeaderContainer>
       </header>
     </HeaderWrapper>
   );
